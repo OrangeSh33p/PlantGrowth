95ae4dff408dae1c6daf906c71f03acd9d550af8
Henri <couvreurhenri@gmail.com>
2019-03-16 18:23:50 +0100

    first prototype

--- Assets/Scripts/Tip.cs
+++ Assets/Scripts/Tip.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 //An extremity of a chunk, that other chunks can attach to
 public class Tip : MonoBehaviour {
-	public List<GameObject> possibleChunks;	//The list of chunk prefabs you can attach to this tip
-	GameObject attachedChunk; //The chunk that is currently attached to the tip (null of none)
+	public List<Chunk> possibleChunks;	//The list of chunk prefabs you can attach to this tip
+
+	public Chunk RandomPossibleChunk () {
+		return possibleChunks[Random.Range(0,possibleChunks.Count)];
+	}
 }
